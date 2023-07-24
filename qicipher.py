a320469ca76233411c1addb3b9c9b1a851366060
pogmel0n <94996665+pogmel0n@users.noreply.github.com>
2023-07-23 20:39:43 -0400

    Add files via upload

---
+++ qicipher.py
@@ -0,0 +1,56 @@
+with open("/Users/fangqi/Documents/snek/pystuff/cryptography/pi.txt") as f:
+    pi = f.read()
+
+check = False
+decrypt = "x"
+
+while check == False:
+    if decrypt == "y":
+        decrypt = True
+        with open("/Users/fangqi/Documents/snek/pystuff/cryptography/ciphertext.txt", "r") as f:
+            word = f.read()
+        check = True
+    elif decrypt == "n":
+        decrypt = False
+        with open("/Users/fangqi/Documents/snek/pystuff/cryptography/plaintext.txt") as f:
+            word = f.read()
+            word = word.replace(" ", "")
+        check = True
+    else:
+        decrypt = input("decrypt: ").lower()
+
+print(word)
+
+key = int(input("key: "))
+
+print(pi[key:(key + (2 * len(word)))])
+
+def encrypt(word, key, decrypt):
+    shiftkey = pi[key:(key + (2 * len(word)))]
+    endword = ""
+    counter = 0
+    for shift in range(0, (len(shiftkey)), 2):
+        s = (int(shiftkey[shift]) * 10 + int(shiftkey[shift + 1])) % 26
+
+        if decrypt:
+            s = 26 - s
+
+        letter = word[counter]
+
+        if ord(letter) < 65 or ord(letter) < 97 and ord(letter) > 90 or 122 < ord(letter):
+                endword += letter
+
+        elif (letter.isupper()):
+                endword += chr((ord(letter) + s - 65) % 26 + 65)
+    
+        else:
+                endword += chr((ord(letter) + s - 97) % 26 + 97)
+
+        counter += 1
+    return endword
+
+print(pi[key:(key + (2 * len(word)))])
+
+f = open("/Users/fangqi/Documents/snek/pystuff/cryptography/ciphertext.txt", "w")
+f.write(encrypt(word, key, decrypt))
+
